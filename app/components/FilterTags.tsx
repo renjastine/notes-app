d5f88c95981c937e43677ebbbd4a7259b5521d0f
ren jastine <rnjstntmj@gmail.com>
2025-11-23 21:36:48 +0800

    Filter tags done

---
+++ app/components/FilterTags.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Tags from './Tags'
+
+function FilterTags() {
+    return (
+        <div className='flex flex-wrap gap-1'>
+            <Tags />
+            <Tags />
+            <Tags />
+            <Tags />
+            <Tags />
+            <Tags />
+            <Tags />
+            <Tags />
+            <Tags />
+        </div>
+    )
+}
+
+export default FilterTags
