0dc3277a9200bd0237626f55240d6e8c28d49ddf
ren jastine <rnjstntmj@gmail.com>
2025-11-23 21:36:40 +0800

    Tags done

---
+++ app/components/Tags.tsx
@@ -0,0 +1,7 @@
+function Tags() {
+    return (
+        <span className='bg-gray-300 text-black/70 text-sm px-3 py-1 rounded-xl h-6 flex justify-center items-center'>tag</span>
+    )
+}
+
+export default Tags
