e25bb1590d42d1a12ee497d642242d391fda5bfc
ren jastine <rnjstntmj@gmail.com>
2025-11-27 22:32:10 +0800

    Dropdown show and hide the items

--- app/components/DropdownItem.tsx
+++ app/components/DropdownItem.tsx
@@ -2,16 +2,13 @@ import Image from "next/image"
 import { useState } from "react"
 
 type DropdownItemProps = {
-    itemName: "new" | "old" | "atoz" | "ztoa"
+    itemName: "new" | "old"
+    selectedSort: string
+    setSelectedSort: (val: string) => void
 }
 
 function DropdownItem({ itemName }: DropdownItemProps) {
-    const [itemText, setItemText] = useState(() => {
-        if(itemName === "old") return "Oldest First"
-        if(itemName === "atoz") return "Title (A-Z)"
-        if(itemName === "ztoa") return "Title (Z-A)"
-        return "Newest First"
-    });
+    const itemText = itemName === "new" ? "Newest First" : "Oldest First"
 
     return (
         <div className="bg-gray-200 p-2 py-1 rounded-md flex justify-between">
--- app/components/DropdownSort.tsx
+++ app/components/DropdownSort.tsx
@@ -1,11 +1,31 @@
 import Image from "next/image";
 import DropdownItem from "./DropdownItem";
+import { useEffect, useRef, useState } from "react";
 
 function DropdownSort() {
+    const [toggleDropdown, setToggleDropdown] = useState(false);
+    const [selectSort, setSelectSort] = useState("new");
+    const dropdown = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        document.addEventListener("click", handleClickOutside)
+        return () => document.removeEventListener("click", handleClickOutside);
+    }, [])
+
+    const handleClickOutside = (e: MouseEvent) => {
+        if (!dropdown.current?.contains(e.target as Node)) setToggleDropdown(false)
+    }
+
+    const handleClick = () => {
+        setToggleDropdown(toggleDropdown ? false : true)
+    }
+
     return (
         <div className="relative">
             <div
+                ref={dropdown}
                 className='font-light text-black/70 flex justify-between gap-15 px-4 py-2 w-[200px] rounded-sm bg-gray-200 mb-1 cursor-pointer select-none'
+                onClick={handleClick}
             >
                 Newest First
                 <Image
@@ -16,12 +36,19 @@ function DropdownSort() {
                     alt="search"
                 />
             </div>
-            <div className="bg-white w-[200px] rounded-md shadow-sm p-1 absolute z-1 hidden">
-                <DropdownItem itemName="new" />
-                <DropdownItem itemName="old" />
-                <DropdownItem itemName="atoz" />
-                <DropdownItem itemName="ztoa" />
-            </div>
+            {toggleDropdown &&
+                <div className="bg-white w-[200px] rounded-md shadow-sm p-1 absolute z-1">
+                    <DropdownItem
+                        selectedSort={selectSort}
+                        setSelectedSort={setSelectSort}
+                        itemName="new"
+                    />
+                    <DropdownItem
+                        selectedSort={selectSort}
+                        setSelectedSort={setSelectSort}
+                        itemName="old"
+                    />
+                </div>}
         </div>
     )
 }
